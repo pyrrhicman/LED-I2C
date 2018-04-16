d956a116a3f19683e0526ecb02edd3633e789f17
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-04-16 22:56:23 +0300

    1

--- LED-I2C-Master C++/LED-I2C-Master C++/main.cpp
+++ LED-I2C-Master C++/LED-I2C-Master C++/main.cpp
@@ -1,21 +1,31 @@
 /*
- * LED-I2C Cplusplus.cpp
- *
  * Created: 4/16/2018 4:48:39 PM
  * Author : Mohammad
  */ 
+
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-
 #include <D:\GitHub\LED-I2C\LED-I2C Lib\i2c_master.c>
 
-
+char data_received;
 int main(void)
 {
+	i2c_init();
+	DDRA = 0;
     /* Replace with your application code */
     while (1) 
     {
+		char key = 0x2f;
+		i2c_start(0x10<<1 | I2C_WRITE);
+		i2c_write(PINA);
+		i2c_stop();
+		_delay_ms(3000);
+		i2c_start(0x10<<1 | I2C_READ);
+		data_received = i2c_read_ack();
+		i2c_read_nack();
+		i2c_stop();
+		
     }
 }
 
---
+++ LED-I2C-Slave C++/LED-I2C-Slave C++/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * LED-I2C-Slave C++.cpp
+ *
+ * Created: 4/16/2018 10:37:25 PM
+ * Author : Mohammad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
