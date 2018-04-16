ecc2083ef6a7ca0efe6a766d535217d4df1f97fd
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-04-16 16:50:08 +0300

    Start

---
+++ LED-I2C Cplusplus/LED-I2C Cplusplus/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * LED-I2C Cplusplus.cpp
+ *
+ * Created: 4/16/2018 4:48:39 PM
+ * Author : Mohammad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
